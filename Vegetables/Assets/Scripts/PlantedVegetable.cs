fa1ae511b0b3166565a40bbec46d784c9af5b244
Tsamf <samfordt@gmail.com>
2023-02-14 18:18:38 -0600

    Cleaned up project files

--- Vegetables/Assets/Scripts/PlantedVegetable.cs
+++ Vegetables/Assets/Scripts/PlantedVegetable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Vegetable : MonoBehaviour
+public class PlantedVegetable : MonoBehaviour
 {
 
     [SerializeField] VegetableSO vegetableInfo;
